fa6f40b61a0a3c69ff3a59c0b9d2da522103c8bf
meszarosmarton1007 <meszarosmarton1007@gmail.com>
2026-06-14 11:27:49 +0200

    tasks adatbázis létrehozása és a hozzá tertozó model és controller

---
+++ database/migrations/2026_06_14_090439_create_tasks_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('tasks', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->text('description')->nullable();
+            $table->dateTime('due_date');
+            $table->string('status')->nullable();
+            //users tábla kapcsolat
+            $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
+            //rekurzív szülő gyerek kapcsolat
+            $table->foreignId('parent_id')->nullable()->constrained('tasks')->onDelete('cascade');
+            $table->timestamps();
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('tasks');
+    }
+};
