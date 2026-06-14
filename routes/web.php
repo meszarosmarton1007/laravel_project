f0b51dbc5ea681bd3dffbec8b7f0bc97c4357174
meszarosmarton1007 <meszarosmarton1007@gmail.com>
2026-06-14 12:58:55 +0200

    vendégnézetek login/regist valamint kijelentkezés utak

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,17 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::post('logout', [AuthController::class, 'Logout'])->name('logout');
+
+Route::middleware('guest')->controller(AuthController::class)->group(function (){
+    Route::get('/register', 'showRegister')->name('show.register');
+    Route::post('/register', 'Register')->name('register');
+    Route::get('/login', 'showLogin')->name('show.login');
+    Route::post('/login', 'Login')->name('login');
 });
