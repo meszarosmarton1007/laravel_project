00dd2913c4e9613fa1f7790aed593e04756644e0
meszarosmarton1007 <meszarosmarton1007@gmail.com>
2026-06-14 12:56:19 +0200

    Task model hozzáadása

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -6,5 +6,22 @@ use Illuminate\Database\Eloquent\Model;
 
 class Task extends Model
 {
-    //
+    protected $fillable = [
+        'title',
+        'description',
+        'due_date',
+        'status',
+        'user_id',
+        'parent_id'
+    ];
+
+    protected $casts = ['due_date' => 'datetime'];
+
+    public function user() {
+        return $this->belongsTo(User::class);
+    }
+
+    public function subtasks(){
+        return $this->hasMany(Task::class, 'parent_id');
+    }
 }
