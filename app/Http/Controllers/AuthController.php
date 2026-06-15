86fbadf936165fd29f5c971d358d88ee7073c4d4
meszarosmarton1007 <meszarosmarton1007@gmail.com>
2026-06-15 15:15:42 +0200

    regisztráció megvalósítása

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,7 +2,11 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Validation\ValidationException as ValidationValidationException;
+use Nette\Schema\ValidationException;
 
 class AuthController extends Controller
 {
@@ -12,5 +16,47 @@ class AuthController extends Controller
 
     public function showLogin(){
         return view('auth.login');
+    }
+
+    
+
+    public function register(Request $request) {
+        $validated = $request->validate([
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|unique:users',
+            'password' => 'required|string|min:8|confirmed'
+        ]);
+
+        $user = User::create($validated);
+
+        Auth::login($user);
+
+        return redirect()->route('tasks.index');
+    }
+
+    public function login(Request $request){
+        $validated = $request->validate([
+            'email' => 'required|email',
+            'password' => 'required|string'
+        ]);
+
+        if(Auth::attempt($validated)){
+            $request->session()->regenerate();
+
+            return redirect()->route('tasks.index');
+        }
+
+        throw ValidationValidationException::withMessages([
+            'credentials' => 'Sorry incorrect credentials'
+        ]);
+    }
+
+    public function logout(Request $request){
+        Auth::logout();
+
+        $request->session()->invalidate();
+        $request->session()->regenerateToken();
+
+        return redirect()->route('show.login');
     }
 }
